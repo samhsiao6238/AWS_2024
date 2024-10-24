4d541e58fe1bbc8066b9f94c0499d48220570ead
samhsiao6238 <samhsiao6238@gmail.com>
2024-10-24 14:22:15 +0800

    Commit on 2024-10-24 14:22:15

--- 01_課程筆記/00_教材/2024學員/素材/ch8RDS/AWSRDS20230810.py
+++ 01_課程筆記/00_教材/2024學員/素材/ch8RDS/AWSRDS20230810.py
@@ -6,41 +6,51 @@ db_settings = {
     "user": "admin",
     "password": "<資料庫密碼>",
     "db": "db20270727",
-    "charset": "utf8"
+    "charset": "utf8",
 }
 try:
     # 建立Connection物件
-    
-    conn=pymysql.connect(**db_settings)
-    
+
+    conn = pymysql.connect(**db_settings)
+
 except Exception as ex:
     print(ex)
-Platform='OBS'
-Synchronize='99'
-Asyn='98'
-Hybrid='92'
+Platform = "OBS"
+Synchronize = "99"
+Asyn = "98"
+Hybrid = "92"
 
 with conn.cursor() as cursor:
-        # 查詢資料SQL語法
-        command = "INSERT INTO ch8 ( `Platform`, `Synchronize`, `Asyn`, `Hybrid`) VALUES ( '"+Platform+"', '"+Synchronize+"', '"+Asyn+"',' "+Hybrid+"')"
-#       command = "INSERT INTO pannel ( `Date`, `Time`, `Lon`, `Lat`, `Alt`) VALUES ( %s, %s, %s, %s, %s);"
-         # 執行指令
-        cursor.execute(command)
-        # 取得所有資料
-        result = cursor.fetchall()
-        print(result)
-        conn.commit()
+    # 查詢資料SQL語法
+    command = (
+        "INSERT INTO ch8 ( `Platform`, `Synchronize`, `Asyn`, `Hybrid`) VALUES ( '"
+        + Platform
+        + "', '"
+        + Synchronize
+        + "', '"
+        + Asyn
+        + "',' "
+        + Hybrid
+        + "')"
+    )
+    #       command = "INSERT INTO pannel ( `Date`, `Time`, `Lon`, `Lat`, `Alt`) VALUES ( %s, %s, %s, %s, %s);"
+    # 執行指令
+    cursor.execute(command)
+    # 取得所有資料
+    result = cursor.fetchall()
+    print(result)
+    conn.commit()
 with conn.cursor() as cursor:
-        # 查詢資料SQL語法
-        command = "SELECT * FROM ch8"
-        # 執行指令
-        cursor.execute(command)
-        # 取得所有資料
-        result = cursor.fetchall()
-        print(result)
-        for row in result:
-            print(row[2])
-        print('result')
-        print(result[1][3])
-#     
+    # 查詢資料SQL語法
+    command = "SELECT * FROM ch8"
+    # 執行指令
+    cursor.execute(command)
+    # 取得所有資料
+    result = cursor.fetchall()
+    print(result)
+    for row in result:
+        print(row[2])
+    print("result")
+    print(result[1][3])
+#
 conn.close()
--- 01_課程筆記/00_教材/2024學員/素材/ch8RDS/awsdbphp.php
+++ 01_課程筆記/00_教材/2024學員/素材/ch8RDS/awsdbphp.php
@@ -1,21 +1,36 @@
 <?php
+/* 定義資料庫連接常量 */
 define('DB_SERVER', 'XXXXXX.XXXXXXl.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'XXXXXX');
 define('DB_DATABASE', 'XXXXXX');
-/* Connect to MySQL and select the database. */
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-if 	(mysqli_connect_errno())  echo "Failed to connect to MySQL: " . mysqli_connect_error();
- else echo "db connected </br>" ;
-$sql="select * from db202208241.ch8";
+
+/* 連接到 MySQL 並選擇資料庫 */
+$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+// 檢查連接是否成功
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit;
+} else {
+    echo "db connected </br>";
+}
+
+// 執行查詢
+$sql = "SELECT * FROM ch8";
 $result = mysqli_query($conn, $sql);
+
+// 檢查查詢是否成功
 if (!$result) {
     echo "DB Error, could not query the database\n";
-    echo 'MySQL Error: ' . mysql_error();
+    echo 'MySQL Error: ' . mysqli_error($conn);  // 使用 mysqli_error 來獲取錯誤訊息
     exit;
 }
-while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-    echo "Platform=".$row['Platform']." ;  Synchronize=".$row['Synchronize']."; Asyn=".$row['Asyn']."; Hybrid= ".$row['Hybrid']."</br>";
+
+// 輸出查詢結果
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    echo "Platform=" . $row['Platform'] . " ;  Synchronize=" . $row['Synchronize'] . "; Asyn=" . $row['Asyn'] . "; Hybrid= " . $row['Hybrid'] . "</br>";
 }
+
+// 關閉連接
 mysqli_close($conn);
-?>
