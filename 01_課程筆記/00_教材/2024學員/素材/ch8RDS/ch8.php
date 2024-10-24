204ae43487b814781a65112944bee7e771f4ca31
samhsiao6238 <samhsiao6238@gmail.com>
2024-10-24 14:11:50 +0800

    Commit on 2024-10-24 14:11:50

--- 01_課程筆記/00_教材/2024學員/素材/ch8RDS/ch8.php
+++ 01_課程筆記/00_教材/2024學員/素材/ch8RDS/ch8.php
@@ -1,21 +1,20 @@
 <?php
 define('DB_SERVER', 'rds20240713.cndjgjxtqom1.us-east-1.rds.amazonaws.com');
 define('DB_USERNAME', 'admin');
-define('DB_PASSWORD', 'USCRDS20240713%');
+define('DB_PASSWORD', '<輸入資料庫密碼>');
 define('DB_DATABASE', 'db20240713');
 /* Connect to MySQL and select the database. */
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
-if  (mysqli_connect_errno())  echo "Failed to connect to MySQL: " . mysqli_connect_error();
- else echo "db connected </br>" ;
-$sql="select * from db20240713.ch8";
+if (mysqli_connect_errno())  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+else echo "db connected </br>";
+$sql = "select * from db20240713.ch8";
 $result = mysqli_query($conn, $sql);
 if (!$result) {
     echo "DB Error, could not query the database\n";
     echo 'MySQL Error: ' . mysql_error();
     exit;
 }
-while ($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
-    echo "Platform=".$row['Platform']." ;  Synchronize=".$row['Synchronize']."; Asyn=".$row['Asyn']."; Hybrid= ".$row['Hybrid']."</br>";
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+    echo "Platform=" . $row['Platform'] . " ;  Synchronize=" . $row['Synchronize'] . "; Asyn=" . $row['Asyn'] . "; Hybrid= " . $row['Hybrid'] . "</br>";
 }
 mysqli_close($conn);
-?>
