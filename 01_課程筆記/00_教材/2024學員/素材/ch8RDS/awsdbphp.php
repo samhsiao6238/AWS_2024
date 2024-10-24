88503247a94e8ad20f2c0c22e96ef63d5655f993
samhsiao6238 <samhsiao6238@gmail.com>
2024-10-24 16:17:09 +0800

    Commit on 2024-10-24 16:17:09

--- 01_課程筆記/00_教材/2024學員/素材/ch8RDS/awsdbphp.php
+++ 01_課程筆記/00_教材/2024學員/素材/ch8RDS/awsdbphp.php
@@ -1,12 +1,20 @@
 <?php
 /* 定義資料庫連接常量 */
-define('DB_SERVER', 'XXXXXX.XXXXXXl.us-east-1.rds.amazonaws.com');
+define(
+    'DB_SERVER',
+    'XXXXXX.XXXXXXl.us-east-1.rds.amazonaws.com'
+);
 define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'XXXXXX');
 define('DB_DATABASE', 'XXXXXX');
 
 /* 連接到 MySQL 並選擇資料庫 */
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$conn = mysqli_connect(
+    DB_SERVER,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_DATABASE
+);
 
 // 檢查連接是否成功
 if (mysqli_connect_errno()) {
@@ -23,7 +31,8 @@ $result = mysqli_query($conn, $sql);
 // 檢查查詢是否成功
 if (!$result) {
     echo "DB Error, could not query the database\n";
-    echo 'MySQL Error: ' . mysqli_error($conn);  // 使用 mysqli_error 來獲取錯誤訊息
+    // 使用 mysqli_error 來獲取錯誤訊息
+    echo 'MySQL Error: ' . mysqli_error($conn);
     exit;
 }
 
